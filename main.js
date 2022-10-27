1fb5c2202654cad4247238e6ea6c1e4956769037
AVKydin <tlalok@ukr.net>
2022-10-27 21:49:05 +0300

    aaray

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 
-let user = { id: 1, name: 'fsdfsdf' };
+// let arr = [123, 223, 345, -123, 0, true, 'dfgfdsgdf'];
 
-let arr = [1, 2, 3, 4, true];
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[2]);
+// console.log(arr[4]);
+// console.log(arr.length);
 
+// arr[0] = 'new'
+
+// console.log(arr);
+
+// arr[7] = 'octen';
+// console.log(arr);
+// arr[arr.length]= '842456465';
+// console.log(arr);
+// arr[arr.length] = "dfdsfd";
+// console.log(arr);
+
+
+// let arr = [
+//     [11, 22, 33],
+//     [],
+//     [],
+// ]
+// console.log(arr);
+
+// console.log(arr[0][1]);
 
