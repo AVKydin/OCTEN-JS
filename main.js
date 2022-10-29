e2c7a45bfaa303a3e68fcdfea8564fabe899f13a
AVKydin <tlalok@ukr.net>
2022-10-29 10:36:27 +0300

    FOR IN loop

--- main.js
+++ main.js
@@ -195,6 +195,34 @@
 //   </div>`)
 // }
 
+// let users = [
+//   { name: "vasya", age: 31, status: false },
+//   { name: "petya", age: 28, status: true },
+//   { name: "kolya", age: 22, status: true },
+//   { name: "olya", age: 27, status: false },
+//   { name: "max", age: 23, status: true },
+//   { name: "anya", age: 35, status: false },
+//   { name: "oleg", age: 34, status: false },
+//   { name: "andrey", age: 29, status: true },
+//   { name: "masha", age: 40, status: true },
+//   { name: "olya", age: 17, status: false },
+//   { name: "max", age: 26, status: true },
+// ];
+
+// for (const user of users) {
+//   console.log(user);
+// }
+
+// let user = {
+//   id: 1,
+//   name: 'vasya',
+//   status: true,
+// }
+
+// for (const filName in user) {
+//     console.log(user[filName]);
+// }
+
 let users = [
   { name: "vasya", age: 31, status: false },
   { name: "petya", age: 28, status: true },
@@ -211,4 +239,9 @@ let users = [
 
 for (const user of users) {
   console.log(user);
+  for (const filName in user) {
+   
+  console.log(filName, user.filName);
+  }
 }
+
