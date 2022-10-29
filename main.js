0eac091a17af91a97e34e4416fcd2155e27b808f
AVKydin <tlalok@ukr.net>
2022-10-29 12:18:24 +0300

    Functional expressions

--- main.js
+++ main.js
@@ -384,13 +384,26 @@
 //   return a + b + c;
 // }
 
-function foobar() {
-  console.log(arguments)
-  if (arguments.length === 2) {
-    return arguments[0] + arguments[1]
-  } else if (arguments.length === 3) {
-    return arguments[0] + arguments[1] +arguments[2];
-  }
+// function foobar() {
+//   console.log(arguments)
+//   if (arguments.length === 2) {
+//     return arguments[0] + arguments[1]
+//   } else if (arguments.length === 3) {
+//     return arguments[0] + arguments[1] +arguments[2];
+//   }
+// }
+//   console.log(foobar(1,2,55));
+
+
+// greeting()      
+// let greeting = function () {
+//     console.log('hi');
+// }
+
+greeting();      
+
+function greeting() {
+  console.log("hi");
 }
-  console.log(foobar(1,2,55));
-      
+
+
