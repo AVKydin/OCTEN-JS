5d2fc687538325661de79f8fcc2440e8d5bbf207
AVKydin <tlalok@ukr.net>
2022-10-29 10:27:20 +0300

    FOR OF loop

--- main.js
+++ main.js
@@ -162,36 +162,53 @@
 // console.log(users[0]);
 
 
-let products = [
-  {
-    title: "milk",
-    price: 22,
-    image: "dcgdfsgdfgdf",
-  },
-  {
-    title: "juice",
-    price: 27,
-    image: "xzc",
-  },
-  {
-    title: "tomato",
-    price: 47,
-    image: "wqewqeqwe",
-  },
-  {
-    title: "tea",
-    price: 15,
-    image: "hjkhjkhj",
-  },
+// let products = [
+//   {
+//     title: "milk",
+//     price: 22,
+//     image: "dcgdfsgdfgdf",
+//   },
+//   {
+//     title: "juice",
+//     price: 27,
+//     image: "xzc",
+//   },
+//   {
+//     title: "tomato",
+//     price: 47,
+//     image: "wqewqeqwe",
+//   },
+//   {
+//     title: "tea",
+//     price: 15,
+//     image: "hjkhjkhj",
+//   },
+// ];
+
+// for (let i = 0; i < products.length; i++){
+//   const product = products[i];
+//   document.write(`
+//   <div>
+//         <h2> ${product.title} - ${product.price} UAH</h2>
+//         <img src="${product.image}">
+  
+//   </div>`)
+// }
+
+let users = [
+  { name: "vasya", age: 31, status: false },
+  { name: "petya", age: 28, status: true },
+  { name: "kolya", age: 22, status: true },
+  { name: "olya", age: 27, status: false },
+  { name: "max", age: 23, status: true },
+  { name: "anya", age: 35, status: false },
+  { name: "oleg", age: 34, status: false },
+  { name: "andrey", age: 29, status: true },
+  { name: "masha", age: 40, status: true },
+  { name: "olya", age: 17, status: false },
+  { name: "max", age: 26, status: true },
 ];
 
-for (let i = 0; i < products.length; i++){
-  const product = products[i];
-  document.write(`
-  <div>
-        <h2> ${product.title} - ${product.price} UAH</h2>
-        <img src="${product.image}">
-  
-  </div>`)
+for (const user of users) {
+  console.log(user);
 }
-
